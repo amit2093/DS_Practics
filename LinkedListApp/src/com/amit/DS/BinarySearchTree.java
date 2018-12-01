453176d4b1c99b8546c88f4b80d5a556bc8e1ca7
amit2093 <er.amit1493@gmail.com>
2018-12-01 15:22:26 +0530

    BST Delete Method.

--- LinkedListApp/src/com/amit/DS/BinarySearchTree.java
+++ LinkedListApp/src/com/amit/DS/BinarySearchTree.java
@@ -1,16 +1,16 @@
 package com.amit.DS;
 
-public class BinarySearchTree {
+class Node{
+	int data;
+	Node left;
+	Node right;
+}
 
-	class Node{
-		int data;
-		Node left, right;
-	}
+public class BinarySearchTree {	
 	
 	public Node insert(Node node, int value) {
-		Node n = null;
 		if(node == null) {
-			n = new Node();
+			Node n = new Node();
 			n.data = value;
 			n.left = null;
 			n.right = null;
@@ -18,12 +18,12 @@ public class BinarySearchTree {
 		}
 		
 		if(value > node.data){
-			n.right = insert(n.right, value);
+			node.right = insert(node.right, value);
 		} else if(value < node.data) {
-			n.left = insert(n.left, value);
+			node.left = insert(node.left, value);
 		}
 		
-		return n;
+		return node;
 	}
 	
 	public Node delete(Node node, int value) {
@@ -33,11 +33,55 @@ public class BinarySearchTree {
 		
 		if(value < node.data) {
 			node.left = delete(node.left, value);
-		} else {
+		} else if(value > node.data) {
 			node.right = delete(node.right, value);
+		} else {
+			if(node.left == null || node.right == null) {
+				Node temp = null;
+				temp = node.left == null ? node.right : node.left;
+				if(temp == null) {
+					return null;
+				} else {
+					return temp;
+				}
+			} else {
+				Node successor = getSuccessor(node);
+				node.data = successor.data;		// copy data
+				node.right = delete(node.right, value); //  check this value
+				return node;
+			}
 		}
 		
-		return null;
+		return node;
 	}
 	
+	private Node getSuccessor(Node node) {
+		if(node == null) {
+			return null;
+		}
+		
+		Node temp = node.right;
+		while(temp.left != null) {
+			temp = temp.left;
+		}
+		
+		return temp;
+	}
+	
+	public static void main(String[] args) {
+		
+		BinarySearchTree bst = new BinarySearchTree();
+		Node root = null;
+		root = bst.insert(root, 8);
+		root = bst.insert(root, 3);
+		root = bst.insert(root, 6);
+		root = bst.insert(root, 10);
+		root = bst.insert(root, 4);
+		root = bst.insert(root, 7);
+		root = bst.insert(root, 1);
+		root = bst.insert(root, 14);
+		root = bst.insert(root, 13);
+		
+		root = bst.delete(root, 1);
+	}
 }
